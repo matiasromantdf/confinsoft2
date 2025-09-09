0b0d58d0042a5dd6cfbdd27825769d3c4bb47f65
Matias Roman <mroman.tdf@gmail.com>
2025-09-08 23:08:11 -0300

    Refactor code structure for improved readability and maintainability

--- dist/assets/PagosYGastosView-BsZVENTo.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,ah as o,a0 as a}from"./index-C4si6ous.js";const e={};function t(c,n,r,p,_,f){return a(),o("div",null," pagos y gastos ")}const l=s(e,[["render",t]]);export{l as default};
