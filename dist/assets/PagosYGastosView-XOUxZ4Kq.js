0b919cda5355811a59bcc80b32069553fac39b51
Matias Roman <mroman.tdf@gmail.com>
2026-01-14 23:00:02 -0300

    v-22

---
+++ dist/assets/PagosYGastosView-XOUxZ4Kq.js
@@ -0,0 +1,1 @@
+import{aa as a,ax as s,ac as o}from"./index-BK9WowpK.js";const e={};function t(c,n,r,p,_,f){return o(),s("div",null," pagos y gastos ")}const l=a(e,[["render",t]]);export{l as default};
