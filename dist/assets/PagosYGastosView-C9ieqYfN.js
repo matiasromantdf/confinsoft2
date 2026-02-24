dba59a1c18a731c07e8ac9f9d0e630c0f1613481
Matias Roman <mroman.tdf@gmail.com>
2026-02-24 00:10:40 -0300

    Refactor code structure for improved readability and maintainability; removed redundant sections and optimized logic flow.

--- dist/assets/PagosYGastosView-C9ieqYfN.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,ak as o,Z as e}from"./index-CNHgCzD7.js";const t={};function a(c,n,r,p,_,f){return e(),o("div",null," pagos y gastos ")}const l=s(t,[["render",a]]);export{l as default};
